6386a16c007595f23e109c34660074f13ae17d5a
WangL <1197647454@qq.com>
2019-10-15 16:27:46 +0800

    删除MyTest文件

--- MyTest.java
+++
@@ -1,7 +0,0 @@
-public class MyTest{
-	public static void main(String[] args){
-			system.out.println("Hello World");
-			system.out.println("Hello World 修改文件");
-			system.out.printin("Hello Wrold 第二次修改文件");
-		}
-	}
