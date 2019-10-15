685e6cd8965c149df31e5b683b6030e6e2b70a9a
WangL <1197647454@qq.com>
2019-10-15 16:16:24 +0800

    第二次修改文件

--- MyTest.java
+++ MyTest.java
@@ -2,5 +2,6 @@ public class MyTest{
 	public static void main(String[] args){
 			system.out.println("Hello World");
 			system.out.println("Hello World 修改文件");
+			system.out.printin("Hello Wrold 第二次修改文件");
 		}
 	}
